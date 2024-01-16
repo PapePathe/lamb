e84a5a579ddb9ad01921b7b193fff2d9f8057709
Pathe SENE <pathe.sene@gmail.com>
2024-01-16 02:11:20 +0000

    feat: cleanup seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,21 +22,21 @@ ecuries = YAML.load_file("#{Rails.root}/lib/app-data/ecuries.yml")
 
 
 ecuries.each do |key, params|
-  
-  
+
+
   puts "creating ecury #{params['nom']}"
   e = Ecury.create! :nom => params['nom'], :ecury_type_id => ecury_type.id
-  
+
   unless params['lutteurs'].nil?
-    
+
     params['lutteurs'].each do |key, params|
       params['ecury_id'] = e.id
-      puts "   adding lutteur: #{params['surnom']} " 
+      puts "   adding lutteur: #{params['surnom']} "
       Lutteur.create! params
     end
-         
+
   end
-  
+
 end
 
 # => Give Pathe Admin Rights
@@ -50,8 +50,8 @@ AdminUser.create! :email => "pathe.sene@xarala.sn", :password => "hotkeyhacker"
   User.create! do |user|
     user.email = Faker::Internet.email
     user.password = "amateur"
-    user.nom_complet = Faker::NameSN.name_sn 
-  end  
+    user.nom_complet = Faker::NameSN.name_sn
+  end
 end
 
 
@@ -60,37 +60,37 @@ booleans = [true, false, false, false]
 # => Populate galas
 puts "creating galas & combats"
 1000.times do
-  
+
   gala = Gala.create! do |g|
     g.nom = "drapeau #{Faker::NameSN.name_sn}"
     g.promoteur = Promoteur.order('rand()').limit(1).first
     g.occurs_at = Random.date_between('2011-10-30'..'2012-05-01')
     g.featured  = booleans[rand booleans.count]
   end
-  
+
   protagonistes = Lutteur.limit(10).order('rand()')
-  
+
   protagonistes.in_groups_of 2 do |challenger, champion|
-    
+
     Combat.create! do |c|
       c.occurs_at    = gala.occurs_at
       c.champion     = champion
       c.challenger   = challenger
       c.fightable    = gala
     end
-    
+
   end
-  
+
 end
 
 Gala.all.each do |gala|
 
  grand_combat = gala.combats.first
- 
+
  grand_combat.featured = true
  grand_combat.grand_combat = true
  grand_combat.save!
-  
+
 end
 
 
--- lib/tasks/dev_data.rake
+++ lib/tasks/dev_data.rake
@@ -1,49 +1,43 @@
 namespace :app do
-  
   namespace :seed do
-  
-    task :galas => :environment do 
-      
+    task :galas => :environment do
+
       [Gala, Combat].each(&:delete_all)
-    
+
       Promoteur.all.each do |promoteur|
-                
+
         gala = Gala.create! do |g|
-          
+
           g.nom = "Drapeau #{Faker::NameSN::name_sn}"
           g.promoteur_id = promoteur.id
           g.occurs_at    = Date.today + (rand(10)+1).weeks
-          g.featured     = true       
-          
+          g.featured     = true
+
         end
-        
+
         5.times do |t|
           Combat.create! do |combat|
             combat.occurs_at = gala.occurs_at
             combat.challenger = Lutteur.order("rand()").first
             combat.champion = Lutteur.where("id != ?", combat.challenger.id).order("rand()").first
-            combat.fightable = gala  
+            combat.fightable = gala
           end
-        end 
-        
+        end
+
         grand_combat = gala.combats.first
         grand_combat.featured = true
         grand_combat.save!
-        
-        
-        
+
+
+
       end
-      
-    end 
-    
-    
-    task :truncate => :environment do      
+
+    end
+
+
+    task :truncate => :environment do
       [Gala, Combat, Ecury, Lutteur, User, Promoteur, AdminUser].each(&:delete_all)
     end
-        
-  end
 
-  
-  
-  
-end 
+  end
+end
